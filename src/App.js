e9f0452f2afe39191b62aa4fb42c8a82282adbb6
memo40 <memocann4040@gmail.com>
2021-01-20 10:11:04 +0100

    addet a random button

--- src/App.js
+++ src/App.js
@@ -14,10 +14,12 @@ function App() {
   axios.get("https://api.imgflip.com/get_memes").then((res)=>{setmeme(res.data.data.memes[value].url)});
  
   axios.get("http://alpha-meme-maker.herokuapp.com/").then((res)=>{console.log(res)})
+  
+  console.log(Math.floor(Math.random()*10))
   return (
     <div className="App">
     <div>type a meme id between 0-100</div>
-    <div><input className="input" type="number" placeholder="type a meme id between 0-100" onChange={(event)=>{setinput(event.target.value)}}></input><input type="submit" value="Submit" onClick={()=>{setvalue(input)}}></input></div>
+    <div><input className="input" type="number" placeholder="type a meme id between 0-100" onChange={(event)=>{setinput(event.target.value)}}></input><input type="submit" value="Generate" onClick={()=>{setvalue(input)}}></input><button onClick={()=>{setvalue(Math.floor(Math.random()*101))}}>Random</button></div>
     
     <div><input type="text" onChange={(event)=>{setmemevalue(event.target.value)}}></input></div>
     <div><button onClick={()=>{setuppertext(memevalue)}}>UpperText</button> <button onClick={()=>{setbottomtext(memevalue)}}>BottomText</button><button onClick={()=>{setuppertext("");setbottomtext("");}}>Clear</button></div>
